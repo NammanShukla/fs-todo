612ccd804099df0f42ac3ed5ac4e71dde5824c4b
NammanShukla <nammanshukla262@gmail.com>
2025-06-26 11:43:31 +0530

    update task routes to use controller functions

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,31 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const Task = require("../models/Task");
+const {
+  getTasks,
+  addTask,
+  updateTask,
+  deleteTask,
+} = require("../controller/taskController");
 
-router.get("/", async (req, res) => {
-  const tasks = await Task.find();
-  res.json(tasks);
-});
-
-
-router.post("/", async (req, res) => {
-  const task = new Task(req.body);
-  const saved = await task.save();
-  res.json(saved);
-});
-
-
-router.put("/:id", async (req, res) => {
-  const updated = await Task.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  res.json(updated);
-});
-
-
-router.delete("/:id", async (req, res) => {
-  await Task.findByIdAndDelete(req.params.id);
-  res.json({ message: "Task deleted" });
-});
+router.get("/", getTasks);
+router.post("/", addTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 module.exports = router;
